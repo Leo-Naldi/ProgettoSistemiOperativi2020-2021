c54f7b3b469d1876b9188e9d3afb35410490ad7a
Leo-Naldi <lnaldi99@gmail.com>
2021-02-07 17:24:32 +0100

    Fixed a typo in ProcSem.h

--- phase1/ProcSem.h
+++ phase1/ProcSem.h
@@ -18,6 +18,6 @@ pcb_PTR outBlocked(pcb_PTR p);
  * Return the pointer at the head of the queue associated at semAdd
  * Return NULL if thw queue is empty or the semAdd doesn't exist
  */
- pcb_PTR headBlocked(int semAdd);
+ pcb_PTR headBlocked(int* semAdd);
 
 #endif
