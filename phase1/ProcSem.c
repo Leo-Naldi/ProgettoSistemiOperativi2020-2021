04671f16a54ed931ba75990bc42bb8174e1782ac
radovic <filipradovic05@gmail.com>
2021-02-11 21:47:33 +0100

    Il secondo tentativo di debugging

--- phase1/ProcSem.c
+++ phase1/ProcSem.c
@@ -77,7 +77,6 @@ int insertBlocked(int *semAdd, pcb_t *p){
   addokbuf("i0\n");
   semd_PTR tmp = searchAdd(semAdd);       /* Fa la ricerca del semaforo con la chiave semAdd */
   addokbuf("i01\n");
-  if(p == NULL) addokbuf("P E' NULL ERRORE\n");
   addokbuf("i1\n");
   if(tmp != NULL){                        /* Questo if gestisce il caso in cui il semaforo e' stato trovato */
     insertProcQ(&(tmp->s_procQ), p);
@@ -86,15 +85,17 @@ int insertBlocked(int *semAdd, pcb_t *p){
   addokbuf("i2\n");
   if(semdFree_h == NULL) return 1;        /* Ritorna TRUE se la lista dei SEMD liberi o inutilizzati e' vuota */ 
   tmp = semdFree_h;                       /* Prende il primo elemento della semdFree per poi inserirlo nell'ASL */
-  semdFree_h = semdFree_h;                /* Aggiorna il head di semdFree */
+  semdFree_h = semdFree_h->s_next;          /* Aggiorna il head di semdFree */
   semd_PTR sliding_tmp = semd_h;          /* Inizializza il puntatore al head dell'ASL neccessario per scorrere la lista 
 					   * fino alla posizione giusta per inserire il nuovo elemento puntato dal semd */  
   addokbuf("i3\n");
-  while(sliding_tmp->s_next != NULL && sliding_tmp->s_next->s_semAdd < semAdd) sliding_tmp = sliding_tmp->s_next;
+  while(sliding_tmp->s_next->s_semAdd < semAdd) sliding_tmp = sliding_tmp->s_next;
   addokbuf("i4\n");
   tmp->s_next = sliding_tmp->s_next;      /* A questo punto si aggiunge tmp nell'ASL fra sliding_tmp e sliding_tmp->s_next */ 
   sliding_tmp->s_next = tmp;
+  addokbuf("i41\n");
   tmp->s_semAdd = semAdd;                 /* L'inizializzazione dei campi di tmp */
+  addokbuf("i42\n");
   tmp->s_procQ = mkEmptyProcQ();
   addokbuf("i5\n");
   insertProcQ(&(tmp->s_procQ), p);
