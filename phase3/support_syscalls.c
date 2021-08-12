e56d08d0cc523563311c4a22ff3db10b80dfc0e6
radovic <filipradovic05@gmail.com>
2021-08-12 15:15:29 +0200

    Il completamento delle syscall

--- phase3/support_syscalls.c
+++ phase3/support_syscalls.c
@@ -1,3 +1,5 @@
+typedef unsigned int devregtr;
+
 /* FUNZIONI PRIVATE */
 
 /*************************************************
@@ -9,7 +11,7 @@
  *************************************************/
 
 static void syscall9(state_t *caller){
-  syscall2(caller);
+  SYSCALL(TERMINATEPROCESS, 0, 0, 0);
 }
 
 /*************************************************
@@ -44,7 +46,26 @@ static void syscall11(state_t *caller){
   if((virtAddr < KUSEG) || (virtAddr > MAXINT) || (len < 0) || (len > 128)){
     syscall9(caller);
   }
-  /* da completare */
+  
+  unsigned int car_tr = 0;
+  unsigned int devno = (((caller->s_entryHI) >> 6) - 1) % 8; // (ASID - 1) % 8
+  devregtr *base = (devregtr*) GET_DEVREG_ADDR(PRNTINT, devno);
+  devregtr status;
+
+  SYSCALL(PASSERN, io_dev_mutex[PRINTER_ROW][devno], 0, 0);
+  while(*virtAddr != EOS){
+    *(base + 3) = PRINTCHR | (((devregtr) *virtAddr) << BYTELEN);
+    status = SYSCALL(WAITIO, PRNTINT, devno, 0);
+    if((status & 0xFF) != 1){ // controlla se device ready
+      SYSCALL(VERHOGEN, io_dev_mutex[PRINTER_ROW][devno], 0, 0);
+      caller->reg_v0 = -1 * status;
+      return;
+    }
+    car_tr++;
+    virtAddr++;
+  }
+  SYSCALL(VERHOGEN, io_dev_mutex[PRINTER_ROW][devno], 0, 0);
+  caller->reg_v0 = car_tr;
 }
 
 /*************************************************
@@ -64,7 +85,26 @@ static void syscall12(state_t *caller){
   if((virtAddr < KUSEG) || (virtAddr > MAXINT) || (len < 0) || (len > 128)){
     syscall9(caller);
   }
-  /* da completare */
+
+  unsigned int car_tr = 0;
+  unsigned int devno = (((caller->s_entryHI) >> 6) - 1) % 8; // (ASID - 1) % 8
+  devregtr *base = (devregtr*) GET_DEVREG_ADDR(TERMINT, devno);
+  devregtr status;
+
+  SYSCALL(PASSERN, io_dev_mutex[TERMW_ROW][devno], 0, 0);
+  while(*virtAddr != EOS){
+    *(base + 3) = PRINTCHR | (((devregtr) *virtAddr) << BYTELEN);
+    status = SYSCALL(WAITIO, TERMINT, devno, 0);
+    if((status & 0xFF) != 5){ // controlla se character transmitted
+      SYSCALL(VERHOGEN, io_dev_mutex[TERMW_ROW][devno], 0, 0);
+      caller->reg_v0 = -1 * status;
+      return;
+    }
+    car_tr++;
+    virtAddr++;
+  }
+  SYSCALL(VERHOGEN, io_dev_mutex[TERMW_ROW][devno], 0, 0);
+  caller->reg_v0 = car_tr;
 }
 
 /*************************************************
@@ -83,5 +123,26 @@ static void syscall13(state_t *caller){
   if((virtAddr < KUSEG) || (virtAddr > MAXINT)){
     syscall9(caller);
   }
-  /* da completare */
+
+  unsigned int car_tr = 0;
+  unsigned int devno = (((caller->s_entryHI) >> 6) - 1) % 8; // (ASID - 1) % 8
+  devregtr *base = (devregtr*) GET_DEVREG_ADDR(TERMINT, devno);
+  devregtr status;
+
+  SYSCALL(PASSERN, io_dev_mutex[TERMR_ROW][devno], 0, 0);
+  while(*virtAddr != EOS){
+    status = SYSCALL(WAITIO, TERMINT, devno, 1);
+    if((status & 0xFF) != 5){ // controlla se character received
+      SYSCALL(VERHOGEN, io_dev_mutex[TERMR_ROW][devno], 0, 0);
+      caller->reg_v0 = -1 * status;
+      return;
+    }
+    *virtAddr = (status >> 8);
+    if(*virtAddr == EOS)
+      break;
+    car_tr++;
+    virtAddr++;
+  }
+  SYSCALL(VERHOGEN, io_dev_mutex[TERMW_ROW][devno], 0, 0);
+  caller->reg_v0 = car_tr;
 }
--- phase3/support_syscalls.h
+++ phase3/support_syscalls.h
@@ -1,6 +1,7 @@
 #ifndef _SUPPORT_SYSCALLS_H_
 #define _SUPPORT_SYSCALLS_H_
 
+#include "proj_lib.h"
 #include "syscalls.h"
 #include "sup_exports.h"
 
