8d114e55d2f8836d9fa72b02b380c26d141ccdae
radovic <filipradovic05@gmail.com>
2021-08-17 12:43:02 +0200

    syscall handler aggiunto e syscall fix

--- phase3/support_syscalls.c
+++ phase3/support_syscalls.c
@@ -49,12 +49,13 @@ static void syscall11(state_t *caller){
   
   unsigned int car_tr = 0;
   unsigned int devno = (((caller->s_entryHI) >> 6) - 1) % 8; // (ASID - 1) % 8
-  devregtr *base = (devregtr*) GET_DEVREG_ADDR(PRNTINT, devno);
+  dtpreg_t *printer = (dtpreg_t*) GET_DEVREG_ADDR(PRNTINT, devno);
   devregtr status;
 
   SYSCALL(PASSERN, io_dev_mutex[PRINTER_ROW][devno], 0, 0);
   while(*virtAddr != EOS){
-    *(base + 3) = PRINTCHR | (((devregtr) *virtAddr) << BYTELEN);
+    printer->command = PRINTCHR;
+    printer->data0 = *virtAddr;
     status = SYSCALL(WAITIO, PRNTINT, devno, 0);
     if((status & 0xFF) != 1){ // controlla se device ready
       SYSCALL(VERHOGEN, io_dev_mutex[PRINTER_ROW][devno], 0, 0);
@@ -131,13 +132,14 @@ static void syscall13(state_t *caller){
 
   SYSCALL(PASSERN, io_dev_mutex[TERMR_ROW][devno], 0, 0);
   while(*virtAddr != EOS){
+    *(base + 1) = PRINTCHR;
     status = SYSCALL(WAITIO, TERMINT, devno, 1);
     if((status & 0xFF) != 5){ // controlla se character received
       SYSCALL(VERHOGEN, io_dev_mutex[TERMR_ROW][devno], 0, 0);
       caller->reg_v0 = -1 * status;
       return;
     }
-    *virtAddr = (status >> 8);
+    *virtAddr = (status >> 8); // faccio shift per ottenere il carattere ricevuto
     if(*virtAddr == EOS)
       break;
     car_tr++;
@@ -145,4 +147,37 @@ static void syscall13(state_t *caller){
   }
   SYSCALL(VERHOGEN, io_dev_mutex[TERMW_ROW][devno], 0, 0);
   caller->reg_v0 = car_tr;
+}
+
+/*****************************************************************************
+ *
+ * Support level syscall handler.
+ *
+ * Utilizza il registro a0 per determinare quale syscall sia stata invocata.
+ * Se il valore in a0 e' >= 14 il current_proc viene ucciso
+ *
+ *****************************************************************************/
+void support_syscall_handler(state_t* caller){
+  unsigned int a0 = caller->reg_a0;
+  
+  if (a0 <= 8 || a0 >= 14) SYSCALL(TERMINATE, 0, 0, 0);
+  
+  caller->pc_epc += 4;
+  switch(a0){
+    case 9:
+      syscall9(caller);
+      break;
+    case 10:
+      syscall10(caller);
+      break;
+    case 11:
+      syscall11(caller);
+      break;
+    case 12:
+      syscall12(caller);
+      break;
+    case 13:
+      syscall13(caller);
+      break;
+  }
 }
